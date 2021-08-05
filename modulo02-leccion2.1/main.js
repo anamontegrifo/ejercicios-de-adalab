57c27470894c559882a547bc68b5dc576b0b89ba
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-05 08:21:41 +0200

    add exercise

--- modulo02-leccion2.1/main.js
+++ modulo02-leccion2.1/main.js
@@ -4,3 +4,15 @@ document.querySelector('h1').innerHTML = 'Esta página no es compatible con la v
 
 let officeAddress = 'Calle Leganitos, 24';
 officeAddress = 'Calle Leganitos, 10';
+
+const mainText = document.querySelector('.text');
+mainText.innerHTML = mainText.innerHTML + ' mundo'
+
+const selectName = document.querySelector('.title');
+const item1 = document.querySelector('.item1');
+const item2 = document.querySelector('.item2');
+selectName.innerHTML = selectName.innerHTML +  item2.innerHTML
+
+
+const password = document.querySelector('.password');
+password.innerHTML =  'Mi contraseña es: **********';
