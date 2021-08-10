8bdd71f5c0839b3cecdaccb1a04ceb88ba9c8ba8
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-10 18:03:43 +0200

    add lesson 2.5

---
+++ modulo02-leccion2.5/main.js
@@ -0,0 +1,16 @@
+'use strict';
+
+//Ejercicio 1
+
+//elementos de HTML
+const button = document.querySelector('.ole');
+const text = document.querySelector('.text');
+//handler
+function changeText() {
+    text.innerHTML = 'Mi primer click, ¡ole yo y la mujer que me parió';
+}
+//listerner
+button.addEventListener('click', changeText);
+
+
+//Ejercicio 2
