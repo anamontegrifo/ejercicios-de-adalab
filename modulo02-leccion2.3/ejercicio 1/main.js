b30a3d31692f49b04acfd301357166c662175207
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-11 12:27:52 +0200

    add exercise

--- modulo02-leccion2.3/ejercicio 1/main.js
+++ modulo02-leccion2.3/ejercicio 1/main.js
@@ -4,6 +4,19 @@ const DEFAULT_AVATAR = 'http://placehold.it/300x300';
 // avatar que eligió el usuario al registrarse
 let userAvatar = 'http://www.fillmurray.com/300/300';
 
+
+
 const image = document.querySelector('.user__avatar');
 image.src = userAvatar || DEFAULT_AVATAR;
+
+//También se puede usar con el atributo setAttribute
+//image.setAttribute("src",userAvatar || DEFAULT_AVATAR);
+
+
+//si lo hacemos con if:
+// if (userAvatar === '') {
+//     image.src = DEFAULT_AVATAR;
+// } else {
+//     image.src = userAvatar;
+// }
 
