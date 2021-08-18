e916d57aa90003f09b73159b3f187971ba549a3a
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-18 14:09:10 +0200

    add exercises 2.2

--- modulo02-leccion2.2/apuntes.js
+++ modulo02-leccion2.2/apuntes.js
@@ -1,2 +1,129 @@
 "use strict";
 
+//Loguear en Consola//
+console.log('Hola');
+
+const age = 56;
+console.log(age);
+
+const welcomeParagraph = document.querySelector('.welcomeText');
+
+// Logueo el contenido de la constante welcomeParagraph en la consola y compruebo que tiene asignado el elemento de HTML que espero antes de cambiar su contenido
+console.log('welcomeParagraph: ', welcomeParagraph);
+
+welcomeParagraph.innerHTML = 'Bienvenida Adalaber';
+
+//Suma, resta, multiplicación y división
+
+1 + 2        // Devuelve 3
+1.4 - 2.4    // Devuelve -1
+    + 7          // Devuelve 7
+    - 40          // Devuelve -40
+    - +8          // Devuelve -8
+30 + 20 - 4  // Devuelve 46
+3 + 3 + 3    // Devuelve 9
+
+4 * 4        // Devuelve 16
+8 * -7       // Devuelve -56
+4 * 2 / 8    // Devuelve 1
+0 / 8        // Devuelve 0
+1.6 / 0.2    // Devuelve 8
+
+4 + 4 * 4 / 8;     // Devuelve 6
+(4 + 4) * 4 / 8;   // Devuelve 4
+(4 + 4) * (4 / 8); // Devuelve 4 también
+
+// + con texto concatena
+const name = 'Paquita';
+const surname = ' Salas';
+console.log(name + surname); // esto pinta en consola 'Paquita Salas'
+
+// + con números suma
+const price = 10;
+const shipping = 3;
+console.log(price + shipping); // esto pinta en consola 13
+
+//String
+//'Esto es un 'bug''
+//"Esto es un "bug""
+'I\'m a front-end developer'
+
+"a"                                   // Devuelve "a"
+'b'                                   // Devuelve "b"
+"Hola"                                // Devuelve "Hola"
+'¿Qué tal?   '                        // Devuelve "¿Qué tal?   " (nótese los espacios)
+"%^%&^%Ω"                             // Devuelve "%^%&^%Ω"
+"Lorem ipsum dolor"                   // Devuelve "Lorem ipsum dolor"
+"123123"                              // Devuelve "123123"
+'"Lorem ipsum dolor sit amet..."'     // Devuelve ""Lorem ipsum dolor sit amet...""
+"\"Lorem ipsum dolor sit amet...\""   // Devuelve ""Lorem ipsum dolor sit amet...""
+'It\'s ok'                            // Devuelve "It's ok"
+
+//Concatenar cadenas
+"Fecha de conexión: " + "jueves 15"  // Devuelve "Fecha de conexión: jueves 15"
+'H' + 'o' + 'l' + 'a'                // Devuelve "Hola"
+"Faltan " + "3" + " días"            // Devuelve "Faltan 3 días"
+
+//Interpolación de cadenas
+//Ejemplo 1
+const stringName = 'Ada';
+const stringSurname = 'Lovelace';
+
+console.log(`My name is ${stringName} ${stringSurname}.`);
+// 'My name is Ada Lovelace.'
+
+//Ejemplo 2
+const firstItemPrice = 5;
+const secondItemPrice = 3;
+
+console.log(`Total amount: ${firstItemPrice + secondItemPrice}€`);
+// Total amount: 8€
+
+//Ejemplo 3
+const element = document.querySelector('#element');
+const textToShow = 'Hey, this is important.';
+
+element.innerHTML = `<div class="popup"><span>${textToShow}</span></div>`;
+
+//Longitud de string
+const browserName = 'Mozilla';
+
+console.log('Mozilla is ' + browserName.length + ' code units long');
+
+//Undefined
+let notDefinedVar;
+
+console.log(notDefinedVar); // undefined
+
+//convertir strings en números con parteInt
+const userAge = document.querySelector('.user__age');
+console.log(userAge.innerHTML); // esto es un string
+const yearsToRetirement = 67 - parseInt(userAge.innerHTML);
+console.log(`Te quedan ${yearsToRetirement} años para jubilarte`);
+
+//Type of
+const text1 = '¿De qué tipo soy?'
+console.log('Soy de tipo:', typeof (text1)) // con paréntesis
+console.log('Soy de tipo:', typeof text1) // sin paréntesis
+
+let text2 = '¿De qué tipo soy?'
+console.log(text2 + ' es del tipo ' + typeof (text2))
+text2 = 123
+console.log('ahora ' + text2 + ' es del tipo ' + typeof (text2))
+text2 = undefined
+console.log('y ahora ' + text2 + ' es del tipo ' + typeof (text2))
+
+
+//Nan
+
+//     `mango` - 2 // NaN
+
+//         `JavaScript loves HTML` * 100  // NaN
+
+// 64 * undefined // NaN
+
+let ladyAge = document.querySelector('.lady__age')
+
+let numberAge = parseInt(ladyAge.value);
+console.log(ladyAge.value);
+console.log('La usuaria no ha introducido una edad válida:', isNaN(numberAge));
--- modulo02-leccion2.2/ejercicios 2.2/main.js
+++ modulo02-leccion2.2/ejercicios 2.2/main.js
@@ -1,36 +1,104 @@
 "use strict";
 
-const item1 = 'Yogures';
-const precio1 = 1.30;
-const item2 = 'Leche';
-const precio2 = 1.05;
-const item3 = 'Tomates';
-const precio3 = 1.20;
+//Ejercicio 1
+console.log(1 + 2);
 
-// const lista = document.querySelector('.js_lista');
-// lista.innerHTML = '<li>' + item1 +  '<span>' + precio1 + '</span>' + '</li>';
+const numberA = 7;
+const numberB = 19;
+
+console.log(numberA + numberB);
 
 
-const lista = document.querySelector('.js_lista');
+//Ejercicio 2
+const superWave = document.querySelector('.super-wave');
+console.log(superWave);
+superWave.classList.add('wave');
+superWave.classList.toggle('wave');
 
-// lista.innerHTML = `<li>${item1} <span>${precio1}</span></li>`;
+//Ejercicio 3
+const priceKiwi = 5;
+const pricePear = 2;
+const priceGrape = 4;
+
+const purchasedKiwi = 2;
+const purchasedPear = 3;
+const pruchasedGrape = 0.5;
+
+const totalKiwi = purchasedKiwi * priceKiwi;
+const totalPear = purchasedPear * pricePear;
+const totalGrape = pruchasedGrape * priceGrape;
+
+const totalPurchase = totalKiwi + totalPear + totalGrape;
+console.log(totalPurchase);
+
+//Ejercicio 4
+
+const sake = 2;
+const account = 128;
+const people = 9;
+
+const personAccount = (account - sake) / people;
+const anaAccount = personAccount + sake;
+console.log('Cada uno debe pagar ' + personAccount + ' euros');
+console.log('Ana debe pagar ' + anaAccount + ' euros');
+
+//Ejercicio 5
+
+let years = 40;
+const daysPerYear = 365;
+const hoursPerDay = 24;
+
+let livedHours = years * daysPerYear * hoursPerDay;
+console.log(livedHours);
+
+years = 99;
+livedHours = years * daysPerYear * hoursPerDay;
+console.log(livedHours);
+
+//ejercicio 6
+
+let partnerName = 'Mara';
+console.log('Hola ' + partnerName + ', encantada de conocerte');
+
+//Ejercicio 7
+
+console.log(`Hola ${partnerName}, encantada de conocerte`);
+
+//Ejercicio 8
+const firstDogImage = 'https://images.dog.ceo/breeds/schipperke/n02104365_8156.jpg';
+const firstDogName = 'Dina';
+
+const secondDogImage = 'https://images.dog.ceo/breeds/collie-border/n02106166_355.jpg';
+const secondDogName = 'Luna';
+
+const thirdDogImage = 'https://images.dog.ceo/breeds/affenpinscher/n02110627_7065.jpg';
+const thirdDogName = 'Lana';
+
+const dogsList = document.querySelector('.dogsList');
+const content = `<li>${firstDogName}</li><img src="${firstDogImage}"><li>${secondDogName}</li><img src="${secondDogImage}"><li>${thirdDogName}</li><img src="${thirdDogImage}">`;
+
+dogsList.innerHTML = content;
+
+//Ejercicio 9
+
+const partnerContainer = document.querySelector('.js-partner');
+let pairName = 'Leticia Fernández Sánchez';
+pairName = 'Sara García';
+
+partnerContainer.innerHTML = `El nombre de mi compañera es ${pairName} y esta compuesto por ${pairName.length} caracteres.`;
+
+//Ejercicio 10
+
+let myAge = document.querySelector('.user__age');
+console.log(myAge.innerHTML);
+const daysXYear = 365;
+const hoursXDay = 24;
+
+let spentHours = parseInt(myAge.innerHTML) * daysXYear * hoursXDay;
+console.log(spentHours);
 
 
-// estas dos formas funcionan igual:
-// lista.innerHTML = lista.innerHTML + `<li>${item2} <span>${precio2}</span></li>`;
-
-// lista.innerHTML += `<li>${item2} <span>${precio2}</span></li>`;
 
 
-let html = `<li>${item1}<span>${precio1}</span></li>`;
-html += `<li>${item2}<span>${precio2}</span></li>`;
-html += `<li>${item3}<span>${precio3}</span></li>`;
 
-const totalIva = precio1 * 0.21 + precio2 * 0.4 + precio3 * 0.4;
-const total = precio1 + precio2 + precio3;
-
-html += `<li>IVA<span>${totalIva}</span></li>`;
-html += `<li>TOTAL<span>${total}</span></li>`;
-
-lista.innerHTML = html;
 
