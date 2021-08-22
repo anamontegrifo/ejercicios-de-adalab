8ad42b82781a34bc4581cb87c0e30fbeae68babd
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-22 09:10:40 +0200

    finish exercise 10

--- modulo02-leccion2.5/ejercicio-10/main.js
+++ modulo02-leccion2.5/ejercicio-10/main.js
@@ -8,15 +8,30 @@ const split = 'Split';
 const button = document.querySelector('.button');
 const moviesList = document.querySelector('.moviesList');
 
-function addMovies(event) {
-    const content = `<li>${inception}</li>`;
+function addMovies() {
+    const content = `<li class="js_movie movieA">${inception}</li><li class="js_movie movieB">${theButterFlyEffect}</li><li class="js_movie movieC">${eternalSunshineOfTheSM}</li><li class="js_movie movieD">${blueVelvet}</li><li class="js_movie movieE">${split}</li>`;
     moviesList.innerHTML = content;
 
+    const movieA = document.querySelector('.movieA');
+    const movieB = document.querySelector('.movieB');
+    const movieC = document.querySelector('.movieC');
+    const movieD = document.querySelector('.movieD');
+    const movieE = document.querySelector('.movieE');
+
+    function printConsole(event) {
+        console.log(event.currentTarget.innerHTML);
+
+    }
+    movieA.addEventListener('click', printConsole);
+    movieB.addEventListener('click', printConsole);
+    movieC.addEventListener('click', printConsole);
+    movieD.addEventListener('click', printConsole);
+    movieE.addEventListener('click', printConsole);
 }
 
-
-
 button.addEventListener('click', addMovies);
+
+
 
 
 
