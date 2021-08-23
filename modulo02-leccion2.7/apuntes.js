30cb60440923efbdb8fe09b2c2b5ce26d0b4b83e
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-23 14:17:19 +0200

    add exercise 4

--- modulo02-leccion2.7/apuntes.js
+++ modulo02-leccion2.7/apuntes.js
@@ -200,11 +200,7 @@ for (let item in userData) {
 
 
 
-//Ejercicio 4
 
-for (let moon = 2017 + 3; moon < 2062; moon = moon + 3) {
-    console.log('La luna del cazador se verá el año:  ' + moon);
-}
 
 //ejercicio 5
 
