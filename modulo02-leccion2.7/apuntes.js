404e7d9140f1efc9372acbe47decc8bc5f202916
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-23 13:31:35 +0200

    add exercise 1

--- modulo02-leccion2.7/apuntes.js
+++ modulo02-leccion2.7/apuntes.js
@@ -193,23 +193,7 @@ for (let item in userData) {
 
 
 
-//Ejercicio 1
-// const movies = ['The 25 hour', 'Moonrise Kingdom', 'Pauline en la playa'];
-// console.log(movies);
-// movies[3] = 'Manhattan';
-// console.log(movies);
-// movies[2] = 'Fargo';
-// console.log(movies);
 
-function workWithMovies() {
-    const movies = ['The 25 hour', 'Moonrise Kingdom', 'Pauline en la playa'];
-    console.log(movies);
-    movies[3] = 'Manhattan';
-    console.log(movies);
-    movies[2] = 'Fargo';
-    console.log(movies);
-}
-workWithMovies();
 
 
 //Ejercicio 2
