312ff1576ca99c81972e4d580dfe66f171d2a311
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-23 12:42:02 +0200

    add exercise 7

---
+++ modulo02-leccion2.6/ejercicio-7/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+const user = {}
+let job = 'developer';
+job = 'profesora';
+
+user.firstName = 'María';
+user.lastName = 'García';
+user['age'] = 30;
+user['job'] = job;
+
+user.firstName = 'Sara';
+user.lastName = 'Pérez';
+user.age = user.age + 1;
+
+
+console.log(`El usuario de llama ${user.firstName} ${user.lastName}, tiene ${user.age} años y es ${user.job}`);
+
