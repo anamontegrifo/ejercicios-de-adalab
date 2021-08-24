9db70c96d5dc3fa030fb0ecdd57de2dcb7d729bb
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-24 12:21:25 +0200

    start exercise 10

---
+++ modulo02-leccion2.7/ejercicio-10-pendiente/main.js
@@ -0,0 +1,18 @@
+'use strict';
+
+//Ejercicio 10
+
+const items = [
+    'Ada',
+    1815,
+    ['Informática', 'Matemática', 'Escritora'],
+    {
+        mother: 'Anna Isabella',
+        father: 'Lord Byron'
+    }
+];
+
+console.table(items);
+
+
+
