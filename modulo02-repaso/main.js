87a1cfa4df0d022341f41e6e37b4b54ce71702d2
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-31 13:14:38 +0200

    review exercise corrections

--- modulo02-repaso/main.js
+++ modulo02-repaso/main.js
@@ -1,27 +1,44 @@
 'use strict';
 
-const container = document.querySelector('.js_container');
-const selectSmile = document.querySelector('.js_select');
+//Called HTML element variables
+const container = document.querySelector('.js-container');
+const selectedSmile = document.querySelector('.js-select');
+const UpdateButton = document.querySelector('.js-update');
+const myFaceToday = document.querySelector('.js-smile');
 
-const UpdateButton = document.querySelector('.js_update');
-const myDay = document.querySelector('.js_smile');
+//Function to change smiley faces
+function updateSmile() {
+    myFaceToday.innerHTML = selectedSmile.value;
 
+    //Otra opción más complicada
+    //const selectedOption = selectedSmile.options[selectedSmile.selectedIndex];
+    //myFaceToday.innerHTML = selectedOption.text;
+}
 
-function UpdateSmile() {
-    const selectedOption = selectSmile.options[selectSmile.selectedIndex];
+//Function to calculate the random number
+function random() {
+    const randomNumber = Math.floor(Math.random() * 100);
+    return randomNumber;
+}
 
-    myDay.innerHTML = selectedOption.text;
+//Function to change the background colour
+function changeColour() {
+    const selectedNumber = random();
+    console.log(selectedNumber);
 
-    let randomNumber = Math.floor((Math.random() * 100) + 1);
-    console.log(randomNumber);
-    if (randomNumber % 2 === 0) {
-        container.classList.remove('redBack');
+    if (selectedNumber % 2 === 0) {
+        container.classList.remove('orangeBack');
         container.classList.add('yellowBack');
     } else {
         container.classList.remove('yellowBack');
-        container.classList.add('redBack');
+        container.classList.add('orangeBack');
     }
 }
 
-UpdateButton.addEventListener('click', UpdateSmile);
+function handleUpdateBtn() {
+    updateSmile();
+    changeColour();
+}
+
+UpdateButton.addEventListener('click', handleUpdateBtn);
 
