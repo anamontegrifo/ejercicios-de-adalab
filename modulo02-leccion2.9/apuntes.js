25ebc7b4551c032eb6bf0386fc1f2d5810cb065e
Ana Montegrifo <amontegrifo@gmail.com>
2021-08-31 20:20:09 +0200

    add exercise 1

---
+++ modulo02-leccion2.9/apuntes.js
@@ -0,0 +1,30 @@
+'use strict';
+
+//MÉTODOS DE ARRAY
+
+
+//PUSH
+const arr = [1, 2, 3];
+const newLength = arr.push(3, 5, 6, 7);
+console.log(newLength); // Loguea 7, la nueva longitud de arr
+console.log(arr); // Loguea 1,2,3,3,5,6,7
+
+var arr2 = [1, 2, 3];
+arr2.push(3, 5, 6, 7, 23, 34, 35, 34, 54, 34, 3434, 34); // Esto es totalmente válido
+console.log(arr2.length);
+console.log(arr2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ modulo02-leccion2.9/ejercicio-1/main.js
@@ -0,0 +1,25 @@
+'use strict';
+
+let oneHundred = [];
+
+function get100Numbers() {
+
+    for (let i = 1; i <= 100; i++) {
+        console.log(oneHundred.push(i))
+    }
+    console.log(oneHundred)
+}
+
+get100Numbers();
+
+
+
+
+
+
+
+
+
+
+
+
