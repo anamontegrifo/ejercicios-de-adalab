7edf998385cbf17c69cce98f420aa65aae59893e
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-02 08:27:48 +0200

    add exercise 2

--- modulo02-leccion2.9/ejercicio-2- pendiente/main.js
+++ modulo02-leccion2.9/ejercicio-2- pendiente/main.js
@@ -1,20 +1,27 @@
 'use strict';
 
 let oneHundred = [];
+let newLength;
 
 function get100Numbers() {
 
     for (let i = 1; i <= 100; i++) {
-        console.log(oneHundred.push(i))
+        oneHundred.push(i)
+        console.log(oneHundred);
     }
-
+    return oneHundred;
 }
-get100Numbers();
 
+let reverse;
 
+function getReverse() {
+    get100Numbers();
+    reverse = oneHundred.reverse();
+    console.log(reverse);
+    return reverse;
+}
 
-
-
+getReverse();
 
 
 
