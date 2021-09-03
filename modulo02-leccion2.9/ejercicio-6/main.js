69780b82272d59231e99500118721b633a486c68
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-03 19:59:15 +0200

    add exercise 6

---
+++ modulo02-leccion2.9/ejercicio-6/main.js
@@ -0,0 +1,18 @@
+'use strict';
+let star = '★';
+let trunk = '|';
+console.log(star);
+let triangle = '';
+
+for (let i = 0; i < 5; i++) {
+    triangle += '▲';
+    console.log(triangle);
+}
+console.log(trunk);
+
+
+
+
+
+
+
