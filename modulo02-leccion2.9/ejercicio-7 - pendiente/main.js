ee21cc7a3e65edd9857ba93b4ed3bde27ff10fe3
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-04 08:38:42 +0200

    update files

---
+++ modulo02-leccion2.9/ejercicio-7 - pendiente/main.js
@@ -0,0 +1,20 @@
+'use strict';
+let star = '    ★';
+let trunk = '    |';
+console.log(star);
+let triangle = '▲';
+let tree;
+
+for (let i = 0; i < 5; i++) {
+
+
+
+}
+console.log(trunk);
+
+
+
+
+
+
+
