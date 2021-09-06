c5d780382c32cd4d3de66023a59b5efb2a917493
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-06 07:52:48 +0200

    add lesson 2.11

--- modulo02-leccion2.9/ejercicio-4 - con fetch/main.js
+++ modulo02-leccion2.9/ejercicio-4 - con fetch/main.js
@@ -4,63 +4,34 @@ const list = document.querySelector('.js-list');
 let tasks = [];
 
 fetch('http://api.igarrido.es/tasks.json')
-    .then(response => response.json()) //esto equivale a .then((response)=>{return response.json();})
-    .then((jsonData) => {
-        tasks = jsonData;
+  .then((response) => response.json()) //esto equivale a .then((response)=>{return response.json();})
+  .then((jsonData) => {
+    tasks = jsonData;
 
-        render();
-    });
-
+    render();
+  });
 
 function handleCheckbox(ev) {
-    console.log(ev.target.parentNode.querySelector('label'));
-    const labelSister = ev.target.parentNode.querySelector('label');
-    labelSister.classList.toggle('completed');
+  console.log(ev.target.parentNode.querySelector('label'));
+  const labelSister = ev.target.parentNode.querySelector('label');
+  labelSister.classList.toggle('completed');
 }
 
 function render() {
-    for (const data of tasks) {
-        //esto equivale a (data.completed === true)
-        if (data.completed) {
-
-            const html = `<li><input type="checkbox" class="js-input" checked><label class="completed">${data.name}</label></li>`;
-            list.innerHTML += html;
-        } else {
-            const html = `<li><input type="checkbox" class="js-input"><label>${data.name}</label></li> `;
-            list.innerHTML += html;
-        }
+  for (const data of tasks) {
+    //esto equivale a (data.completed === true)
+    if (data.completed) {
+      const html = `<li><input type="checkbox" class="js-input" checked><label class="completed">${data.name}</label></li>`;
+      list.innerHTML += html;
+    } else {
+      const html = `<li><input type="checkbox" class="js-input"><label>${data.name}</label></li> `;
+      list.innerHTML += html;
     }
-    const allInput = document.querySelectorAll('.js-input');
-    console.log(allInput);
+  }
+  const allInput = document.querySelectorAll('.js-input');
+  console.log(allInput);
 
-    for (const eachInput of allInput) {
-        eachInput.addEventListener('click', handleCheckbox);
-    }
+  for (const eachInput of allInput) {
+    eachInput.addEventListener('click', handleCheckbox);
+  }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
