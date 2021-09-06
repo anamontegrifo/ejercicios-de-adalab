0c034ad443ece5dfb94e180bd903f3445009ef8d
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-06 17:34:22 +0200

    add exercise 2

--- modulo02-leccion2.11/ejercicio-2/main.js
+++ modulo02-leccion2.11/ejercicio-2/main.js
@@ -1,20 +1,35 @@
 'use strict';
 
-const search = document.querySelector('.js-search').value;
+const search = document.querySelector('.js-search');
 const btn = document.querySelector('.js-star');
+const list = document.querySelector('.js-list');
+let content = '';
 
-function findCharacter(ev) {
-	const textSearch = search;
-	console.log(search);
-	let url = 'https://swapi.dev/api/people/?search=' + textSearch;
-	console.log(url);
+function findCharacter() {
+	const textSearch = search.value;
+	let url = `https://swapi.dev/api/people/?search=${textSearch}`;
 
 	fetch(url)
 		.then((result) => result.json())
 		.then((data) => {
-			console.log(data);
-			console.log(data.results[0].name);
+			console.log(data); // devuelve los elementos que coincide con la búsqueda como objeto
+			console.log(data.results[0].name); //devuelve el nombre del elemento el la posición 0
 			console.log(data.results[0].gender);
+
+			const characters = data.results;
+			console.log(characters); //devuelve el array de los elementos que coinciden con la búsqueda
+
+			for (const name of characters) {
+				if (name.gender === 'female') {
+					const characterText = `<li>${name.name} es un personaje de género  femenino</li>`;
+					content += characterText;
+				} else {
+					const characterText = `<li>${name.name} es un personaje de género masculino</li>`;
+					content += characterText;
+				}
+
+				list.innerHTML = content;
+			}
 		});
 }
 
