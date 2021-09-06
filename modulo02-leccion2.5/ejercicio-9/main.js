b7872c932d2d0c5e180f4ea1548d60f546a92a33
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-06 12:30:11 +0200

    add improvements

--- modulo02-leccion2.5/ejercicio-9/main.js
+++ modulo02-leccion2.5/ejercicio-9/main.js
@@ -1,22 +1,45 @@
 'use strict';
 
-const profileA = document.querySelector('.teacher--isra');
-const profileB = document.querySelector('.teacher--tuerto');
-const profileC = document.querySelector('.teacher--nasi');
+//------------------------------------------------------------------//
+//Versión elemento por elemento
+
+// const profileA = document.querySelector('.teacher--isra');
+// const profileB = document.querySelector('.teacher--tuerto');
+// const profileC = document.querySelector('.teacher--nasi');
+
+// function changeStyle(event) {
+// 	event.currentTarget.classList.toggle('teacher--selected');
+
+// 	const addOrRemove = event.currentTarget.querySelector('.favorite');
+// 	if (addOrRemove.innerHTML === 'Añadir') {
+// 		addOrRemove.innerHTML = 'Quitar';
+// 	} else {
+// 		addOrRemove.innerHTML = 'Añadir';
+// 	}
+// }
+
+// profileA.addEventListener('click', changeStyle);
+// profileB.addEventListener('click', changeStyle);
+// profileC.addEventListener('click', changeStyle);
+
+//------------------------------------------------------------------//
+
+//Versión con querySelectorAll
+const list = document.querySelectorAll('.teacher');
 
 function changeStyle(event) {
+	const teacher = event.currentTarget;
+	teacher.classList.toggle('teacher--selected');
 
-    event.currentTarget.classList.toggle('teacher--selected');
-
-    const addOrRemove = event.currentTarget.querySelector('.favorite');
-    if (addOrRemove.innerHTML === 'Añadir') {
-        addOrRemove.innerHTML = 'Quitar';
-    }
-    else {
-        addOrRemove.innerHTML = 'Añadir';
-    }
+	const addOrRemove = teacher.querySelector('.favorite');
+	console.log(addOrRemove);
+	if (addOrRemove.innerHTML === 'Añadir') {
+		addOrRemove.innerHTML = 'Quitar';
+	} else {
+		addOrRemove.innerHTML = 'Añadir';
+	}
 }
 
-profileA.addEventListener('click', changeStyle);
-profileB.addEventListener('click', changeStyle);
-profileC.addEventListener('click', changeStyle);
+for (const item of list) {
+	item.addEventListener('click', changeStyle);
+}
