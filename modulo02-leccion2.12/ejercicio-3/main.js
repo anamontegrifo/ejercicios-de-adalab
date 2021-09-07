64a447d6194cb63e92c39733abd285dbd8d82aec
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-07 08:26:50 +0200

    start exercise 3

---
+++ modulo02-leccion2.12/ejercicio-3/main.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const users = [
+	{
+		name: 'Rosario',
+		surName: 'García',
+		phone: '876947342',
+	},
+	{
+		name: 'Susana',
+		surName: 'Martínez',
+		phone: '650349821',
+	},
+	{
+		name: 'Julia',
+		surName: 'Maroto',
+		phone: '239482749',
+	},
+];
+
+console.log(users[0].name);
