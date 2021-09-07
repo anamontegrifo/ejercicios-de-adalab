274f0277950f5f2646853bf09a7d83472b327847
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-07 21:49:40 +0200

    add exercise 1

--- modulo02-leccion2.13/apuntes.js
+++ modulo02-leccion2.13/apuntes.js
@@ -1,1 +1,22 @@
 'use strict';
+
+//MÉTODOS FUNCIONALES DE ARRAY
+
+//MAP
+//Comparando bucle normal
+const names = ['María', 'Lucía', 'Susana', 'Rocío', 'Inmaculada'];
+const capitalNames = [];
+
+for (let i = 0; i < names.length; i++) {
+	const capitalName = names[i].toUpperCase();
+	capitalNames.push(capitalName);
+}
+console.log(capitalNames);
+
+//Usando map
+const capitalName = names.map((name) => name.toUpperCase()); //con arrow function integrada
+console.log(capitalName);
+//otro ejemplo declarando una función y luego la aplica
+const getUperCaseName = (name) => name.toUpperCase();
+const capitalNames2 = names.map(getUperCaseName);
+console.log(capitalNames);
