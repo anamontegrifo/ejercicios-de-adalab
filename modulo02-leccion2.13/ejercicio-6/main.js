7ee9eaae65fbe72adce212187a03d1a78ed964ef
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-08 08:29:17 +0200

    add exercise 6

---
+++ modulo02-leccion2.13/ejercicio-6/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const users = [
+	{ name: 'María', isPremium: false, pin: 2389 },
+	{ name: 'Lucía', isPremium: true, pin: 2384 },
+	{ name: 'Susana', isPremium: true, pin: 2837 },
+	{ name: 'Rocío', isPremium: false, pin: 5232 },
+	{ name: 'Inmaculada', isPremium: false, pin: 8998 },
+];
+
+const evenPins = users.filter((user) => {
+	if (user.pin % 2 === 0) {
+		return user;
+	}
+});
+
+console.log(evenPins);
