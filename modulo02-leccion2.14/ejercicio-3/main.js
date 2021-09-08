5acb4a7b4fb4ab6b99fb1ec8476d8e5f41b12728
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-08 20:49:00 +0200

    modify exercise 3

--- modulo02-leccion2.14/ejercicio-3/main.js
+++ modulo02-leccion2.14/ejercicio-3/main.js
@@ -1,30 +1,13 @@
 'use strict';
 
-//1º opción
-
 let counter = 0;
 const textArea = document.querySelector('.js-text');
 
 const howLongAgo = () => {
 	counter++;
 	textArea.innerHTML = `Escrito hace ${counter} segundos`;
-	if (counter === 60) {
+	if (counter >= 60) {
 		textArea.innerHTML = `Escrito hace un minuto`;
-		clearInterval(temp);
 	}
 };
-
-temp = setInterval(howLongAgo, 1000);
-
-//2º opción
-// let counter = 1;
-// const textArea = document.querySelector('.js-text');
-
-// const howLongAgo = setInterval(function () {
-// 	textArea.innerHTML = `Escrito hace ${counter} segundos`;
-// 	counter++;
-// 	if (counter > 60) {
-// 		textArea.innerHTML = `Escrito hace un minuto`;
-// 		clearInterval(howLongAgo);
-// 	}
-// }, 1000);
+setInterval(howLongAgo, 1000);
