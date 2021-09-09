6d4b3609c416844a3554f34918817083e3cc7720
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-09 09:55:57 +0200

    add changes

--- modulo02-leccion2.4/ejercicio-7/main.js
+++ modulo02-leccion2.4/ejercicio-7/main.js
@@ -3,35 +3,30 @@
 //Ejercicio 7
 
 function getEl(selector) {
-    const shortHand = document.querySelector(selector);
+	const shortHand = document.querySelector(selector);
 
-    if (shortHand === null) {
-        console.error(`No existe ningún elemento con clase, id o tag llamado ${shortHand}`);
-    }
-    else {
-        return shortHand;
-    }
+	if (shortHand === null) {
+		console.error(
+			`No existe ningún elemento con clase, id o tag llamado ${shortHand}`
+		);
+	} else {
+		return shortHand;
+	}
 }
 
 let textEl = getEl('.text');
 let newNumber = parseInt(textEl.innerHTML);
 
-textEl.innerHTML = 24;
+textEl.innerHTML = 33;
 newNumber = textEl.innerHTML;
 
 console.log(newNumber);
 
 function oddAndEven(a) {
-
-    if (a % 2 === 0) {
-        console.log(`${a} is an even number`);
-    }
-    else {
-        console.log(`${a} is not an even number`);
-    }
+	if (a % 2 === 0) {
+		console.log(`${a} is an even number`);
+	} else {
+		console.log(`${a} is not an even number`);
+	}
 }
 oddAndEven(newNumber);
-
-
-
-
