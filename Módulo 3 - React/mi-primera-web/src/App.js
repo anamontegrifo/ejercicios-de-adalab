e4ef28f5853eeeda17c15cd518b6939ed530bc14
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-17 08:18:28 +0200

    exercise 1 & 2

--- Módulo 3 - React/mi-primera-web/src/App.js
+++ Módulo 3 - React/mi-primera-web/src/App.js
@@ -2,24 +2,25 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className="App">
+			<header className="App-header">
+				<img src={logo} className="App-logo" alt="logo" />
+				<p>
+					Edit <code>src/App.js</code> and save to reload.
+				</p>
+				<p>Módulo 3.1 Ejercicio 1</p>
+				<a
+					className="App-link"
+					href="https://reactjs.org"
+					target="_blank"
+					rel="noopener noreferrer"
+				>
+					Aprende React
+				</a>
+			</header>
+		</div>
+	);
 }
 
 export default App;
