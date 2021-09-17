de2386894d2e2f34ce07833708e92655bfaf86a6
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-17 21:58:09 +0200

    add exercises lesson 1.5

---
+++ Módulo 3 - React/React - lección 1.5/src/components/App.js
@@ -0,0 +1,51 @@
+import logo from '../images/adalab-logo-155x61.png';
+import '../styles/App.css';
+
+function App() {
+	return (
+		<div>
+			<header className="header-section">
+				<a href="https://adalab.es/">
+					<img
+						className="logo-adalab"
+						title="logo Adalab"
+						src={logo}
+						alt="logo adalab"
+					/>
+				</a>
+				<nav>
+					<ul className="nav-section">
+						<li className="li-items">
+							<a
+								className="nav-link"
+								href="https://adalab.es/blog/"
+								title="Acceso a blog"
+								target="_blank"
+								rel="noreferrer"
+							>
+								blog
+							</a>
+						</li>
+						<li className="li-items">
+							<a
+								className="nav-link"
+								href="https://adalab.es/contacto/"
+								title="Acceso a contacto"
+								target="_blank"
+								rel="noreferrer"
+							>
+								contacto
+							</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+
+			<main className="main-section">
+				<h1 className="main-text">Creando diversidad digital</h1>
+			</main>
+		</div>
+	);
+}
+
+export default App;
