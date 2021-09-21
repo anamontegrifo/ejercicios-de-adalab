cd48f18077f134f6b8528399a741b10689498493
Ana Montegrifo <amontegrifo@gmail.com>
2021-09-21 19:50:49 +0200

    add exercise 2

---
+++ Módulo 3 - React/React - lección 4.1 - ejercicio 2/src/components/App.js
@@ -0,0 +1,35 @@
+import '../styles/App.scss';
+
+function App() {
+	const tasks = [
+		{ task: 'Comprar harina, jamón y pan rallado', completed: true },
+		{ task: 'Hacer croquetas ricas', completed: true },
+		{ task: 'Ir a la puerta de un gimnasio', completed: true },
+		{
+			task: 'Comerme las croquetas mirando a la gente que entra en el gimnasio',
+			completed: false,
+		},
+	];
+
+	const renderTasks = () => {
+		return tasks.map((task, index) => {
+			if (task.completed === true) {
+				return (
+					<li className="completed" key={index}>
+						{task.task}
+					</li>
+				);
+			} else {
+				return <li key={index}>{task.task}</li>;
+			}
+		});
+	};
+	return (
+		<div>
+			<h1>Lista de tareas</h1>
+			<ul>{renderTasks()}</ul>
+		</div>
+	);
+}
+
+export default App;
